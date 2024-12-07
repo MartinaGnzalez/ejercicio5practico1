27894d9839e2c8a50b066bc956f2dc5a6ae4ea09
marti <martinagoncalvezz@gmail.com>
2024-12-07 15:28:33 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio5practico1/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5practico1
+{
+    internal class Matriz
+    {
+        public int filas;
+        public int columnas;
+        public int[,] matrizInterna;
+
+        public Matriz(int nuevofilas, int nuevocolumnas)
+        {
+            filas = nuevofilas;
+            columnas = nuevocolumnas;
+            matrizInterna = new int[filas, columnas];
+            rellenarMatrizConValoresAleatorios();
+
+        }
+
+        private void rellenarMatrizConValoresAleatorios()
+        {
+            Random numero = new Random();
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+
+                    matrizInterna[i, j] = numero.Next(9,205);
+                }
+            }
+        }
+        public Matriz sumarMatrices (Matriz nueva) 
+        { 
+            if (filas != nueva.filas || columnas != nueva.columnas)
+            {
+                throw new Exception("Las matrices deben tener las misma dimensiones para poder sumarlas.");
+            }
+
+            Matriz resultado = new Matriz(filas, columnas);
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0;j < columnas; j++)
+                {
+                    resultado.matrizInterna[i, j] = this.matrizInterna[i, j] + nueva.matrizInterna[i, j];
+                }
+            }
+            return resultado;
+
+           
+        }
+    }
+}
+
+
+
---
+++ ejercicio5practico1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5practico1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matriz matriz1 = new Matriz(3, 3);
+            Matriz matriz2 = new Matriz(3, 3);
+
+            ImprimirMatriz(matriz1);
+            Console.WriteLine();
+            ImprimirMatriz(matriz2);
+
+            Matriz resultado = matriz1.sumarMatrices(matriz2);
+
+            Console.WriteLine("Resultado de la suma:");
+            ImprimirMatriz(resultado);
+        }
+
+        static void ImprimirMatriz(Matriz m)
+        {
+            for (int i = 0; i < m.filas; i++)
+            {
+                for (int j = 0; j < m.columnas; j++)
+                {
+                    Console.Write(m.matrizInterna[i, j] + "");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+        
